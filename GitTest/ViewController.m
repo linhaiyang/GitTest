27a428c1762707142906b41748936d65c52540a9
linhaiyang <qq26393157@163.com>
2020-10-28 21:14:46 +0800

    gitstore

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -19,6 +19,7 @@
     //yyyyyyy  hhjjjhhj
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"");
+    self.view.backgroundColor = [UIColor orangeColor];
 }
 
 
